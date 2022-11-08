7fe3da44019d632d64a94c86636144b43e366b8a
matthewjmckinley <matthewjamesmckinley@gmail.com>
2022-11-07 17:01:37 -0800

    Fix slidenav button bug in Thanks Roy

--- TimelinePlugin.php
+++ TimelinePlugin.php
@@ -52,6 +52,7 @@ class TimelinePlugin extends Omeka_Plugin_AbstractPlugin
     public function hookPublicHead()
     {
         queue_css_url('https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css');
+        queue_css_file('timeline');
         queue_js_url('https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js');
     }
 
