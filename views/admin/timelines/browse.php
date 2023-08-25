cae449d12ce8ed3c7604fd99e45eefde581c373d
matthewjmckinley <matthewjamesmckinley@gmail.com>
2023-08-25 11:36:15 -0700

    Open public timeline page in new window

--- views/admin/timelines/browse.php
+++ views/admin/timelines/browse.php
@@ -18,7 +18,7 @@ echo head($head);
     </p>
 <?php endif; ?>
 <p id="message" style="color: green;"></p>
-<p>Drag and drop timelines in the order you would like them displayed on the <a href="<?php echo public_url('timeline') ?>">public timeline page</a></p>
+<p>Drag and drop timelines in the order you would like them displayed on the <a href="<?php echo html_escape(public_url('timeline')); ?>" target="_blank">public timeline page</a>.</p>
 <ul id="sortable" class="ui-sortable">
     <?php foreach ($timelines as $timeline): ?>
     <li id="timelines-<?php echo html_escape($timeline->id) ?>" class="ui-state-default sortable-item"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
