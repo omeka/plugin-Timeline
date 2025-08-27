d54a2b32e952182e97c461ad07d81445fce2a2f2
Kim Nguyen <design@kimisgold.com>
2025-08-27 11:11:46 -0400

    Remove unused arrow span.

--- views/admin/timelines/browse.php
+++ views/admin/timelines/browse.php
@@ -21,7 +21,7 @@ echo head($head);
 <p>Drag and drop timelines in the order you would like them displayed on the <a href="<?php echo html_escape(public_url('timeline')); ?>" target="_blank">public timeline page</a>.</p>
 <ul id="sortable" class="ui-sortable">
     <?php foreach ($timelines as $timeline): ?>
-    <li id="timelines-<?php echo html_escape($timeline->id) ?>" class="ui-state-default sortable-item drawer"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
+    <li id="timelines-<?php echo html_escape($timeline->id) ?>" class="ui-state-default sortable-item drawer">
         <span class="move icon" aria-label="<?php echo __('Move'); ?>" title="<?php echo __('Move'); ?>"></span>
         <span class="timeline-title drawer-name"><?php echo link_to($timeline, 'show', $timeline->title); ?></span>
         <div class="other-meta">
